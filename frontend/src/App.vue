8136f809b3a40b7aab5f30466519ff24cfd4ce0b
wwwy-source <ziwen5379@gmail.com>
2026-07-08 20:15:42 +0800

    feat(frontend): 更新登录页面布局和大厅页面功能卡片位置

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,57 @@
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import HomePage from './HomePage.vue'
 import ScriptPage from './ScriptPage.vue'
 import ArchivePage from './ArchivePage.vue'
 import RolePage from './RolePage.vue'
+import LoginRegister from './LoginRegister.vue'
 
-const currentPage = ref('大厅')
+const PAGE_HOME = 'home'
+const PAGE_SCRIPT = 'script'
+const PAGE_ARCHIVE = 'archive'
+const PAGE_ROLE = 'role'
+
+const currentPage = ref(PAGE_HOME)
 const selectedWorldview = ref(null)
+const isAuthenticated = ref(false)
+
+const pageComponentMap = {
+  [PAGE_HOME]: HomePage,
+  [PAGE_SCRIPT]: ScriptPage,
+  [PAGE_ARCHIVE]: ArchivePage,
+  [PAGE_ROLE]: RolePage
+}
+
+const pageAliasMap = {
+  home: PAGE_HOME,
+  大厅: PAGE_HOME,
+  script: PAGE_SCRIPT,
+  世界观: PAGE_SCRIPT,
+  archive: PAGE_ARCHIVE,
+  档案: PAGE_ARCHIVE,
+  role: PAGE_ROLE,
+  角色: PAGE_ROLE
+}
+
+const activeComponent = computed(() => pageComponentMap[currentPage.value] || HomePage)
 
 const handleNavigate = (page, worldview = null) => {
-  currentPage.value = page
+  currentPage.value = pageAliasMap[page] || PAGE_HOME
   selectedWorldview.value = worldview
+}
+
+const handleEnterApp = () => {
+  isAuthenticated.value = true
 }
 </script>
 
 <template>
-  <HomePage v-if="currentPage === '大厅'" :current-page="currentPage" @navigate="handleNavigate" />
-  <ScriptPage v-else-if="currentPage === '世界观'" :current-page="currentPage" @navigate="handleNavigate" />
-  <ArchivePage v-else-if="currentPage === '档案'" :current-page="currentPage" @navigate="handleNavigate" />
-  <RolePage v-else-if="currentPage === '角色'" :current-page="currentPage" :worldview="selectedWorldview" @navigate="handleNavigate" />
+  <LoginRegister v-if="!isAuthenticated" @enter="handleEnterApp" />
+  <component
+    :is="activeComponent"
+    v-else
+    :current-page="currentPage"
+    :worldview="selectedWorldview"
+    @navigate="handleNavigate"
+  />
 </template>
-
